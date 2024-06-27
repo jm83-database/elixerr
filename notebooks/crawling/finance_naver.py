14f10be70f7e8747f88e131981bb3b4ca6bd77a4
jinmyung <jinmyung0803@naver.com>
2024-06-27 20:24:00 +0900

    finance_naver, real_estate analysis created

--- notebooks/crawling/finance_naver.py
+++ notebooks/crawling/finance_naver.py
@@ -7,57 +7,64 @@ plt.rc('font', family='Malgun Gothic') # 차트에 한글 글자 깨짐 방지
 
 
 
+def get_exchange_rate_data(selected):
+    df_selected = pd.DataFrame()
+    for page_num in range(1,11,1):
+        url = f'https://finance.naver.com/marketindex/exchangeDailyQuote.naver?marketindexCd=FX_{selected}KRW&page={page_num}'
+        temp_num = pd.read_html(url, encoding='cp949', header = 1)
+        df_selected = pd.concat([df_selected,temp_num[0]])
+    total_rate_data_view(df_selected)
+
+
+def total_rate_data_view(df_selected):
+    # 원하는 열만 선택
+    df_selected = df_selected.drop(['전일대비'],axis=1)
+    #데이터 재정렬
+    df_selected = df_selected[::-1].reset_index(drop=True)
+    #데이터 표시
+    print(f'''==={currency_name[currency_code-1]} - {selected}({currency_symbols[currency_code-1]})===
+    {df_selected.head(10)}
+    ''')
+
+    # 전체 차트 작성
+    df_total_chart = df_selected.copy()
+    df_total_chart = df_total_chart.set_index('날짜')
+    # df_total_chart = df_total_chart[::-1]
+    df_total_chart ['매매기준율'].plot(figsize=(15,6), title = 'exchange rate') # figsize 단위는 inch
+    plt.show()
+    month_rate_data_view(df_selected)
+
+
+def month_rate_data_view(df_selected):
+    # 월별 차트 작성
+# 날짜 데이터 str → datetime 변환
+    df_selected['날짜'] = df_selected['날짜'].str.replace(".",'').astype('datetime64[ms]')
+
+    # 월 파생변수 생성
+    df_selected['월'] = df_selected['날짜'].dt.month
+    month_in = int(input('검색할 월입력(예:9): '))
+    month_df = df_selected.loc[df_selected['월'] == month_in].drop('월', axis=1)
+
+    # 월별 데이터 표시
+    print(f'''==={currency_name[currency_code-1]} - {selected}({currency_symbols[currency_code-1]})===
+    {month_df.head(10)}
+    ''')
+
+    # 차트를 그릴 데이터셋 복제
+    month_df_chart = month_df.copy()
+    month_df_chart = month_df_chart.set_index('날짜')
+
+    # 선택한 달의 매매기준율 차트 그리기
+    month_df_chart['매매기준율'].plot()
+    plt.show()
+    
+
+
+
 # 여러 국가의 환율을 1~10페이지까지 추출
-currency_code = int(input('통화유형을 선택해주세요(0:USD, 1:EUR, 2:JPY): '))
+currency_code = int(input('통화유형을 선택해주세요(1:USD, 2:EUR, 3:JPY): '))
 currency_type = ['USD', 'EUR', 'JPY']
 currency_name = ['미국 달러', '유럽연합 유로', '일본 엔(100)']
 currency_symbols = ['$','€', '¥']
-selected = currency_type[currency_code]
-df_selected = pd.DataFrame()
-for page_num in range(1,11,1):
-    url = f'https://finance.naver.com/marketindex/exchangeDailyQuote.naver?marketindexCd=FX_{selected}KRW&page={page_num}'
-    temp_num = pd.read_html(url, encoding='cp949', header = 1)
-    df_selected = pd.concat([df_selected,temp_num[0]])
-
-
-    
-# 원하는 열만 선택
-df_selected = df_selected.drop(['전일대비'],axis=1)
-#데이터 재정렬
-df_selected = df_selected[::-1].reset_index(drop=True)
-#데이터 표시
-print(f'''==={currency_name[currency_code]} - {selected}({currency_symbols[currency_code]})===
-{df_selected.head(10)}
-''')
-
-
-
-# 전체 차트 작성
-df_total_chart = df_selected.copy()
-df_total_chart = df_total_chart.set_index('날짜')
-# df_total_chart = df_total_chart[::-1]
-df_total_chart ['매매기준율'].plot(figsize=(15,6), title = 'exchange rate') # figsize 단위는 inch
-plt.show()
-
-
-# 월별 차트 작성
-# 날짜 데이터 str → datetime 변환
-df_selected['날짜'] = df_selected['날짜'].str.replace(".",'').astype('datetime64[ms]')
-
-# 월 파생변수 생성
-df_selected['월'] = df_selected['날짜'].dt.month
-month_in = int(input('검색할 월입력(예:9): '))
-month_df = df_selected.loc[df_selected['월'] == month_in].drop('월', axis=1)
-
-# 월별 데이터 표시
-print(f'''==={currency_name[currency_code]} - {selected}({currency_symbols[currency_code]})===
-{month_df.head(10)}
-''')
-
-# 차트를 그릴 데이터셋 복제
-month_df_chart = month_df.copy()
-month_df_chart = month_df_chart.set_index('날짜')
-
-# 선택한 달의 매매기준율 차트 그리기
-month_df_chart['매매기준율'].plot()
-plt.show()
+selected = currency_type[currency_code-1]
+df_selected = get_exchange_rate_data(selected)
---
+++ notebooks/crawling/real_estate_func.py
@@ -0,0 +1,73 @@
+# pandas 이용
+import pandas as pd
+import warnings
+import matplotlib.pyplot as plt
+warnings.filterwarnings('ignore') # 파일 오류 무시
+plt.rc('font', family='Malgun Gothic') # 차트에 한글 글자 깨짐 방지
+
+def load_data():
+    # 1~13페이지까지 추출
+    df = pd.DataFrame()
+    for page_num in range(1,14,1):
+        url = f'https://land.naver.com/news/trendReport.naver?page={page_num}'
+        temp_num = pd.read_html(url, encoding='utf-8')
+        df = pd.concat([df,temp_num[0]])
+
+    return df
+
+def clean_data(df):
+    # % 제거
+    df_cp = df.copy()
+    df_cp = df_cp['제목'].str.replace('%', "")
+    # 전국, 서울, 수도권 제거
+    regions = ['전국', '서울', '수도권']
+    for region in regions:
+        df_cp = df_cp.str.replace(region, '')
+
+    # ]를 중심으로 컬럼 구분 후 필요한 컬럼 선택
+    df_cp = df_cp.str.split(']', expand=True)
+    df_cp = df_cp[1]
+
+    # ,를 기준으로 분리
+    df_cp = df_cp.str.split(',', expand=True)
+
+    # 선택된 정보 저장
+    df_selected = df_cp.copy()
+
+    # str → float 자료형 변환
+    df_selected = df_selected.astype('float')
+
+    # 컬럼명 바꾸기
+    df_selected = df_selected.rename(columns={0:'전국', 1:'서울', 2:'수도권'})
+
+    # 데이터 병합
+    df[regions] = df_selected
+
+    # 데이터 선택
+    df_rate = df[['등록일']+regions+['번호']]
+
+    # 데이터 정렬
+    df_rate = df_rate[::-1]
+    return df_rate
+
+def draw_chart(df_rate):
+    # 차트 그리기
+    df_rate.head(30).plot(x = '등록일', y = ['전국', '서울', '수도권'], figsize = (15,6), title = '2019.07~2020.01 부동산 가격 추이')
+    plt.show()
+
+    # datetime 변환
+    df_rate['등록일'] = pd.to_datetime(df_rate['등록일'])
+
+    # 월별 추이 분석(평균)
+    montly_analysis = df_rate.groupby(df_rate.등록일.dt.month)[['전국', '서울', '수도권']].mean()
+    montly_analysis.plot(figsize = (15,6), title = '월별 부동산 가격 추이(평균)')
+    plt.show()
+
+    # 년도별 추이 분석(평균)
+    yearly_analysis = df_rate.groupby(df_rate.등록일.dt.year)[['전국', '서울', '수도권']].mean()
+    yearly_analysis.plot(figsize = (15,6), title = '년도별 부동산 가격 추이(평균)')
+    plt.show()
+    
+df = load_data()
+df_rate = clean_data(df)
+print(draw_chart(df_rate))
