e92a06e442b8d0042d0f7b68aad7224c475416f3
jinmyung <jinmyung0803@naver.com>
2024-08-07 09:18:40 +0900

    펀드수익계산.ipynb created

--- notebooks/deploy/중앙갈치.py
+++ notebooks/deploy/중앙갈치.py
@@ -1,11 +1,14 @@
 import datetime
 import pandas as pd
 
+print("환영합니다.\n2주 단위로 쉬는 일요일을 알려주는 프로그램입니다.")
 # 원하는 식당명을 받습니다.
-restaurant_id = input("원하는 식당명을 입력해주세요 : ")
+restaurant_id = input("입력하고 싶은 식당명을 입력해주세요 : ")
 
-# 기준일을 지정합니다.
-startday = datetime.datetime.strptime("2024-07-21", '%Y-%m-%d')
+# 기준일을 입력합니다.
+startday_input = input("기준일을 입력해주세요(예)20240721) : ")
+startday_fomatting = startday_input[0:4] + '-' + startday_input[4:6] + '-' + startday_input[6:]
+startday = datetime.datetime.strptime(startday_fomatting, '%Y-%m-%d')
 # 오늘 날짜를 가져옵니다.
 today = datetime.datetime.today()
 
@@ -14,10 +17,15 @@ deltatime = today - startday
 deltatime_to_int = deltatime.days # range에 넣기 위해 int로 변환
 
 sundays = []
-# 오늘부터 2주 단위로 일요일을 표시합니다.
-for i in range(0, deltatime_to_int+7, 14):
+# 오늘을 기준으로 최근 쉬었던 일요일을 찾아줍니다.
+for i in range(0, deltatime_to_int, 14):
     sunday = startday + datetime.timedelta(days=i)
     sundays.append(sunday)
 
-result = sundays[-1]
-print(f'{restaurant_id}이/가 쉬는 가까운 일요일은 {result.year}-{result.month}-{result.day}입니다.')
+past_sunday_off = sundays[-1]
+next_sunday_off = past_sunday_off + datetime.timedelta(days=14)
+
+text = f'''{restaurant_id}이/가 쉬었던 가까운 일요일은 {past_sunday_off.year}-{past_sunday_off.month}-{past_sunday_off.day}입니다.
+다가오는 쉬는 날은 {next_sunday_off.year}-{next_sunday_off.month}-{next_sunday_off.day}입니다.
+'''
+print(text)
